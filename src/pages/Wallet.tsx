16b697158d7b77be63c9b7f8473d191c265c890a
franklinemose19-alt <franklinemose19@gmail.com>
2026-07-06 20:58:34 +0300

    Create Wallet.tsx

---
+++ src/pages/Wallet.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function Wallet() {
+  return <div className="page"><div className="title" style={{ marginTop: 40 }}>Wallet</div><div className="text-dim">Coming next.</div></div>
+}
