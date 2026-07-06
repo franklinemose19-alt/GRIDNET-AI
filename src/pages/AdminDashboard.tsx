1b8524d3569b62126739be0a32ee2305c545fc1f
franklinemose19-alt <franklinemose19@gmail.com>
2026-07-06 21:01:16 +0300

    Add AdminDashboard component

---
+++ src/pages/AdminDashboard.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function AdminDashboard() {
+  return <div className="page"><div className="title" style={{ marginTop: 40 }}>Admin</div><div className="text-dim">Coming next.</div></div>
+}
