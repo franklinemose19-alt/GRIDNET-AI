ea875143a5367077d36c8d55082e1fd21d5483b1
franklinemose19-alt <franklinemose19@gmail.com>
2026-07-06 21:00:32 +0300

    Create ProviderDashboard.tsx

---
+++ src/pages/ProviderDashboard.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function ProviderDashboard() {
+  return <div className="page"><div className="title" style={{ marginTop: 40 }}>Provider Dashboard</div><div className="text-dim">Coming next.</div></div>
+}
