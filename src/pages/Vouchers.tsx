ee52832244d15fcf36753b5bdde126d922bd39d8
franklinemose19-alt <franklinemose19@gmail.com>
2026-07-06 20:59:15 +0300

    Add Vouchers component with placeholder content

---
+++ src/pages/Vouchers.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function Vouchers() {
+  return <div className="page"><div className="title" style={{ marginTop: 40 }}>Vouchers</div><div className="text-dim">Coming next.</div></div>
+}
