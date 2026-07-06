0d47d414dae2eae3a074b4c1b9bc1679bea53e1d
franklinemose19-alt <franklinemose19@gmail.com>
2026-07-06 20:59:48 +0300

    Create ActiveSession.tsx

---
+++ src/pages/ActiveSession.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function ActiveSession() {
+  return <div className="page"><div className="title" style={{ marginTop: 40 }}>Session</div><div className="text-dim">Coming next.</div></div>
+}
